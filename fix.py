2b697fba6d9c4f205ef7754d0c0a8805132d459d
StoicGang <ganeshghadge1414@gmail.com>
2026-05-09 20:11:17 +0530

    removed unnecessary files

--- fix.py
+++
@@ -1,8 +0,0 @@
-
-import sqlite3
-conn = sqlite3.connect('data/vault.db')
-conn.row_factory = sqlite3.Row
-rows = conn.execute(\"SELECT id, title, tags FROM entries WHERE is_deleted = 0\").fetchall()
-for r in rows:
-    print(dict(r))
-conn.close()
